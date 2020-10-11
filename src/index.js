ad8d0b0441aab8e70bc6c97b94dadf132ecf7153
nadavCodex <nadav@codex.co.il>
2020-10-11 13:49:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { createBrowserHistory } from "history";
 
 import reducers from "./js/store/reducers/reducers";
 import Root from "./js/Root";
@@ -15,17 +14,13 @@ const rootReducer = combineReducers(reducers);
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const customHistory = createBrowserHistory();
-
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Suspense
         fallback={<div style={{ textAlign: "center", margin: "70px auto" }} />}
       >
-        <Router history={customHistory}>
           <Root />
-        </Router>
       </Suspense>
     </BrowserRouter>
   </Provider>
